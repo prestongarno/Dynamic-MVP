b1183e349202b61d3b69b09b2c56ceb8412d0ba1
Preston Garno <prestongarno@gmail.com>
2017-03-09 21:12:57 -0500

    removed ignored files

--- ExampleApplication/build/generated/source/apt/main/edu/gvsu/prestongarno/example/runnerGenerated.java
+++
@@ -1,8 +0,0 @@
-package edu.gvsu.prestongarno.example;
-public class runnerGenerated {
-//got them niggas!
-	private static final String secretString = "This thing is fucking awesome!";
-	public static String getHackedString(){
-		return runnerGenerated.secretString;
-	}
-}
